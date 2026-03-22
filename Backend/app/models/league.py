2c9f7f61a4f4476c98c0757659095d912fa13d88
Jsatchell03 <jsatchell2017@gmail.com>
2026-03-22 17:36:24 -0400

    Added ORM models

--- Backend/app/models/league.py
+++ Backend/app/models/league.py
@@ -1,5 +1,25 @@
-from abc import ABC
+import uuid
+from sqlalchemy import Column, Text, Boolean, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID, JSONB, TIMESTAMP
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from .base import Base
 
 
-class League(ABC):
-    pass
+class League(Base):
+    __tablename__ = "leagues"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(Text, nullable=False)
+    location = Column(Text)
+    sport_id = Column(UUID(as_uuid=True), ForeignKey("sports.id"), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    rules = Column(JSONB, nullable=False)
+    logo_location = Column(Text)
+    is_community_sourced = Column(Boolean, nullable=False)
+
+    postseason_tournament_id = Column(UUID(as_uuid=True), ForeignKey("tournaments.id"))
+    current_season_id = Column(UUID(as_uuid=True), ForeignKey("seasons.id"))
+
+    seasons = relationship("Season", back_populates="league")
+    teams = relationship("Team", back_populates="league")
--- Backend/app/models/team.py
+++ Backend/app/models/team.py
@@ -1,5 +1,29 @@
-from abc import ABC
+import uuid
+from sqlalchemy import Column, Text, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID, TIMESTAMP
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from .misc import division_teams, organization_teams
+from .base import Base
 
 
-class Team(ABC):
-    pass
+class Team(Base):
+    __tablename__ = "teams"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(Text, nullable=False)
+    main_league_id = Column(UUID(as_uuid=True), ForeignKey("leagues.id"))
+    logo_location = Column(Text)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    sport_id = Column(UUID(as_uuid=True), ForeignKey("sports.id"))
+    venue_id = Column(UUID(as_uuid=True), ForeignKey("venues.id"))
+
+    league = relationship("League", back_populates="teams")
+    fixtures = relationship("FixtureTeam", back_populates="team")
+    divisions = relationship(
+        "Division", secondary=division_teams, back_populates="teams"
+    )
+
+    organizations = relationship(
+        "Organization", secondary=organization_teams, back_populates="teams"
+    )
