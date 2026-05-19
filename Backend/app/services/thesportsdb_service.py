c93fcfe92e65285fd0139f79eb685db00f6c77f8
Jsatchell03 <jsatchell2017@gmail.com>
2026-05-19 18:57:39 -0400

    No model parsing in TSDB service, simply make request and return relevant data in a dict. No badge uploading is done in TSDB service. Stick to single responsibility.

--- Backend/app/services/thesportsdb_service.py
+++ Backend/app/services/thesportsdb_service.py
@@ -20,20 +20,6 @@ class TheSportsDBError(Exception):
     pass
 
 
-def upload_badge(res, name):
-    logo_url = res.get("strBadge", None)
-    if not logo_url:
-        logo_location = None
-    else:
-        logo_name = name.lower().replace(" ", "_") + "_badge"
-        logo_location = upload_from_url(logo_url, "scorza-provider-assets", logo_name)
-    return logo_location
-
-
-def parse_team(res):
-    pass
-
-
 def v2_request(url, type):
     try:
         response = requests.get(url, headers=HEADERS, timeout=TIMEOUT)
@@ -60,123 +46,65 @@ def v2_request(url, type):
         raise TheSportsDBError(f"Request failed: {e}")
 
 
-def v2_request_decorator(func):
-    @functools.wraps(func)
-    def wrap(*args, **kwargs):
-        try:
-            request = func(*args, **kwargs)
-            response = requests.get(request["url"], headers=HEADERS, timeout=TIMEOUT)
-            response.raise_for_status()
-            data = response.json()
-
-            if not data or not data.get(request["type"]):
-                raise TheSportsDBError(data.get("Message", "Empty response from API"))
-            if request["type"] == "lookup":
-                return data.get(request["type"])[0]
-            else:
-                return data.get(request["type"])
-
-        except requests.exceptions.HTTPError as e:
-            raise TheSportsDBError(f"HTTP error: {e}")
-
-        except requests.exceptions.ConnectionError:
-            raise TheSportsDBError("Connection error")
-
-        except requests.exceptions.Timeout:
-            raise TheSportsDBError("Request timed out")
-
-        except requests.exceptions.RequestException as e:
-            raise TheSportsDBError(f"Request failed: {e}")
-
-    return wrap
-
-
-"""
-These functions simply contruct the url and return an object with the information
-the wrapper needs to execute and handle errors.
-"""
-
-
-@v2_request_decorator
-def search_league_name(name):
-    url = f"{BASE_URL}/v2/json/search/league/{name}"
-    request_type = "search"
-    return {"url": url, "type": request_type}
-
-
 def get_league(id):
     url = f"{BASE_URL}/v2/json/lookup/league/{id}"
     request_type = "lookup"
-    res = v2_request(url, request_type)
+    response = v2_request(url, request_type)
 
-    if not res.get("strLeague", None):
-        raise TheSportsDBError("Incomplete League")
+    if not response.get("strLeague", None):
+        raise TheSportsDBError(f"TSDB league:{id} missing 'strLeague'.")
 
-    return League(
-        id=None,
-        name=res["strLeague"],
-        location=res.get("strCountry", None),
-        logo_location=upload_badge(res, res["strLeague"]),
-        rules=None,
-        is_community_sourced=False,
-        sport_id=None,
-    )
+    return {
+        "id": response["idLeague"],
+        "name": response["strLeague"],
+        "location": response.get("strCountry", None),
+        "badge": response.get("strBadge", None),
+    }
 
 
 def get_league_teams(id):
     url = f"{BASE_URL}/v2/json/list/teams/{id}"
     request_type = "list"
-    res = v2_request(url, request_type)
+    response = v2_request(url, request_type)
+    teams = []
+    incomplete_teams = None
+
+    for team in response:
+        if not team.get("strTeam", None):
+            incomplete_teams = 1 if incomplete_teams is None else incomplete_teams + 1
+            continue
+        teams.append(
+            {
+                "id": team["idTeam"],
+                "name": team["strTeam"],
+                "league_id": team.get("idLeague", None),
+                "badge": team.get("strBadge", None),
+                "location": team.get("strCountry"),
+            }
+        )
+
+    if incomplete_teams:
+        print(f"{incomplete_teams} incomplete teams")
+
+    return teams
 
 
-@v2_request_decorator
 def get_team(id):
     url = f"{BASE_URL}/v2/json/lookup/team/{id}"
     request_type = "lookup"
-    return {"url": url, "type": request_type}
+    response = v2_request(url, request_type)
 
+    if not response.get("strTeam", None):
+        raise TheSportsDBError(f"TSDB team:{id} missing 'strTeam'.")
 
-@v2_request_decorator
-def get_team_schedule(id):
-    url = f"{BASE_URL}/v2/json/schedule/full/team/{id}"
-    request_type = "schedule"
-    return {"url": url, "type": request_type}
-
-
-@v2_request_decorator
-def get_event_streaming(id):
-    url = f"{BASE_URL}/v2/json/lookup/event_tv/{id}"
-    request_type = "lookup"
-    return {"url": url, "type": request_type}
-
-
-@v2_request_decorator
-def get_fixture(id):
-    url = f"{BASE_URL}/v2/json/lookup/event/{id}"
-    request_type = "lookup"
-    return {"url": url, "type": request_type}
-
-
-@v2_request_decorator
-def get_fixture_lineup(id):
-    url = f"{BASE_URL}/v2/json/lookup/event_lineup/{id}"
-    request_type = "lookup"
-    return {"url": url, "type": request_type}
-
-
-@v2_request_decorator
-def get_fixture_stats(id):
-    url = f"{BASE_URL}/v2/json/lookup/event_stats/{id}"
-    request_type = "lookup"
-    return {"url": url, "type": request_type}
-
-
-@v2_request_decorator
-def get_venue(id):
-    url = f"{BASE_URL}/v2/json/lookup/venue/{id}"
-    request_type = "lookup"
-    return {"url": url, "type": request_type}
+    return {
+        "id": response["idTeam"],
+        "name": response["strTeam"],
+        "league_id": response.get("idLeague", None),
+        "badge": response.get("strBadge", None),
+        "location": response.get("strCountry"),
+    }
 
 
 if __name__ == "__main__":
-    print(get_league_teams(4328))
+    print(get_team(133601))
